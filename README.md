c9f4c20a98f406ad4a8cf14b15b30046241ab4d5
Ana Fede <ana.fede@uconn.edu>
2023-01-13 12:51:52 -0500

    revising files

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+# Work Day Scheduler
+
+## Description
 This repo contains a responsive schedule app for a regular 9-5 workday; a user can log their daily activities by the hour. I created this using bootsrap and jQuery. The page is responsive with the time; when the hour is in the future it will appear green, when the hour is in the past it will appear grey, and when the hour is present it will appear red. I've included a screenshot of it below. <img width="1080" alt="Screen Shot 2022-10-18 at 9 08 40 PM" src="https://user-images.githubusercontent.com/112656003/196573675-2b11af28-5a2d-4c2f-89a9-54e0526b5975.png">
--- script.js
+++ script.js
@@ -1,50 +1,33 @@
-function updateClock() {
-    $('#currentDay').html(moment().format('MMMM Do YYYY, h:mm:ss a'));
-}
-setInterval(updateClock, 1000);
+var today = moment().format('LLL');
+$("#currentDay").text(today);
+
+var currentTime = moment().format('HH');
 
 
 $(document).ready(function(){
-$(".saveBtn").on("click", function () {
-     console.log(this);
-    var text = $(this).siblings(".description").val(); 
-    var time = $(this).parent().attr("id");
-    localStorage.setItem  (time,text);
-})
-
-$("#hour9 .description").val(localStorage.getItem("hour9"));
-$("#hour10 .description").val(localStorage.getItem("hour10"));
-$("#hour11 .description").val(localStorage.getItem("hour11"));
-$("#hour12 .description").val(localStorage.getItem("hour12"));
-$("#hour13 .description").val(localStorage.getItem("hour13"));
-$("#hour14 .description").val(localStorage.getItem("hour14"));
-$("#hour15 .description").val(localStorage.getItem("hour15"));
-$("#hour16 .description").val(localStorage.getItem("hour16"));
-$("#hour17 .description").val(localStorage.getItem("hour17"));
-
-function tracker(){
-     var timeNow = moment().hour();
-
-    $(".time-block").each(function(){
-        var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+    $(".saveBtn").on("click", function () {
+         console.log(this);
+        var text = $(this).siblings(".description").val(); 
+        var time = $(this).parent().attr("id");
+        localStorage.setItem  (time,text);
+    })});
     
-        
-    if (timeBlock < timeNow){
-        $(this).removeClass("future");
-        $(this).removeClass("present");
-        $(this).addClass("past");
-     }  
-    else if (timeBlock === timeNow) {
-        $(this).removeClass("future");
-         $(this).removeClass("past");
-         $(this).addClass("present");
-     }  
-    else {
-         $(this).removeClass("present");
-         $(this).removeClass("past");
-         $(this).addClass("future");
-     }
-    })
-}
-     tracker();
-})
+
+   $(".time-block").each(function () {
+        var userInput = parseInt($(this).attr("id"));
+
+        if (userInput > currentTime) {
+            $(this).addClass("future")
+
+        } else if (userInput == currentTime) {
+
+            $(this).addClass("present")
+
+        }
+        else {
+            $(this).addClass("past")
+
+        };
+
+        $("#" + userInput).val(localStorage.getItem(userInput))
+    });
